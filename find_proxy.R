bba9b1f770652a4a9c47698f3bf159d9de504b3e
linjf15 <83997468+linjf15@users.noreply.github.com>
2022-03-22 17:09:30 +0800

    Create find_proxy.R
    find_proxy is a function built for searching for proxy SNP in the "http://rest.ensembl.org/"[for built 38] or "http://grch37.rest.ensembl.org/" [for build 37]

---
+++ find_proxy.R
@@ -0,0 +1,25 @@
+find_proxy <- function(snp = "rs10001", r2_threshold = 0.8, build = "37")
+{
+  stopifnot(build %in% c("37","38"))
+  
+  # Create and get a url
+  server <- ifelse(build == "37","http://grch37.rest.ensembl.org","http://rest.ensembl.org")
+  ext <- paste0("/ld/human/",snp,"/1000GENOMES:phase_3:EUR")
+  url <- paste(server, ext, sep = "")
+  print(paste0("searching proxies for ",snp," ......"))
+  res <- httr::GET(url, httr::content_type("application/json"))
+  
+  # Converts http errors to R errors or warnings
+  httr::stop_for_status(res)
+  
+  # Convert R objects from JSON
+  res <- httr::content(res)
+  proxy_df <- jsonlite::fromJSON(jsonlite::toJSON(res))
+  proxy_df <- lapply(proxy_df, unlist)
+  
+  # Extract SNP associated with targeted SNP with r2 > r2_threshold
+  snp_r2 <- proxy_df$r2[proxy_df$r2>r2_threshold]
+  snp_v <- proxy_df$variation2[proxy_df$r2>r2_threshold]
+  
+  return(snp_v[order(snp_r2, decreasing=T)])
+}
